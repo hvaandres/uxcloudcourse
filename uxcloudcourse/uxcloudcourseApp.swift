437a13fefb7475d6ea9f3315b5506f47bbc9e73a
Andres Haro <hvaandres@icloud.com>
2022-03-09 22:46:11 -0700

    Initial Commit

---
+++ uxcloudcourse/uxcloudcourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  uxcloudcourseApp.swift
+//  uxcloudcourse
+//
+//  Created by Andres Haro on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct uxcloudcourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
