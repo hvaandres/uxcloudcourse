a7c1b0cfd209e4c1f2c2b1030fdddd5b832dc9c4
Andres Haro <hvaandres@icloud.com>
2022-03-16 22:42:48 -0600

    adding a better style to the cards from the content

--- uxcloudcourse/ContentView.swift
+++ uxcloudcourse/ContentView.swift
@@ -11,19 +11,24 @@ struct ContentView: View {
     var body: some View {
         VStack (alignment: .leading, spacing: 8.0){
             Spacer()
+            // Adding Logo
             Image("Logo 2")
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
                 .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
-                
+            // Adding the title
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.accentColor, .accentColor.opacity(1.2)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            // Subtitle for the section
             Text("20 Sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
-                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
+            
+            // Description
             Text("Build an iOS app for iOS 15 with custom layouts animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
@@ -34,11 +39,23 @@ struct ContentView: View {
         .padding(.all, 20.0)
         .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(Color("Background"))
+        .background(.ultraThinMaterial)
         .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
         .shadow(color: Color("Shadow").opacity(0.5), radius: 10, x: 0, y: 10)
         .padding(.horizontal, 20)
+        .background(Image("Blob 1")
+                        .offset(x:250, y:-100)
+        )
+        
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+        )
     }
+       
 }
 
 struct ContentView_Previews: PreviewProvider {
